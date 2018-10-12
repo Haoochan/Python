786196e276ad0e80f4f254d88ddfa8660508aeef
Haoochan <673055417@qq.com>
2018-10-12 22:29:38 +0800

    Revert "Revert "10-12 19.21""
    This reverts commit 9d37bb91edd51e62b775fcd4fe696cfc5d86a936.

---
+++ Weibo/t1.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import re
+import datetime
+import time
+
+
+# now_time = datetime.datetime.now()
+# now1 = now_time.strftime('%Y-%m-%d %H:%M:%S')
+# print(now1)
+# time.sleep(2)
+# now_time = datetime.datetime.now()
+# now2 = now_time.strftime('%Y-%m-%d %H:%M:%S')
+# print(now2)
+# print(now2-now1)
+# now3=now_time+datetime.timedelta(minutes=-1)
+# print(now3.strftime('%Y-%m-%d %H:%M:%S'))
+
+s='第30页'
+print(s[1:-1])
